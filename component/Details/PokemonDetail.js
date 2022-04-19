4c5e0b7f8cc7b8516f045371ea800556d1002731
khalidibwal <khalidibwal@gmail.com>
2022-04-19 19:57:27 +0700

    add animation into aps

--- component/Details/PokemonDetail.js
+++ component/Details/PokemonDetail.js
@@ -1,12 +1,35 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, Image, StyleSheet, ActivityIndicator} from 'react-native';
-
+import React, {useState, useEffect, useRef} from 'react';
+import {View, Text, Image, StyleSheet, ActivityIndicator, Animated, Dimensions} from 'react-native';
+import { Card, Button } from '@rneui/themed';
+import { TouchableOpacity } from 'react-native-web';
 const PokemonDetail = props => {
   const [details, setDetails] = useState([]);
+
+  const windowWidth = Dimensions.get('window').width;
+  const initPosition = {
+    x: parseInt(windowWidth / 2) - 50,
+    y: 0,
+  };
+
+  const position = useRef(new Animated.ValueXY(initPosition)).current;
+
+  const animate = () => {
+    Animated.spring(position, {
+      toValue: {x: initPosition.x, y: -450},
+      useNativeDriver:false,
+      speed: 4,
+     
+    }).start(() => {
+      position.setValue(initPosition);
+    });
+    alert('Better Luck Next Time')
+  };
 
   useEffect(() => {
     fetchPokemonDetails();
   }, []);
+
+
 
   const fetchPokemonDetails = () => {
     const pokemons = props.route.params.pokemon
@@ -35,7 +58,14 @@ const PokemonDetail = props => {
         Moves: {details.moves[0].move.name}
       </Text>
       <Text style={styles.text}>Type: {details.types[0].type.name}</Text>
+      <View style={styles.container}>
+      <Animated.View style={position.getLayout()}>
+        <Image source={require('./../../assets/pokeball.png')} style={styles.ball}/>
+      </Animated.View>
+      <Button onPress={animate} title="Catch Pokemon" style={styles.btnStyle} />
     </View>
+    </View>
+    
   ) : (
     <View style={styles.indicator}>
       <ActivityIndicator size="large" color="#E63F34" />
@@ -59,4 +89,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container: {
+    flex: 1,
+  },
+  ball: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    backgroundColor: 'red',
+  },
+  btnStyle:{
+    borderRadius:20
+  }
 });
